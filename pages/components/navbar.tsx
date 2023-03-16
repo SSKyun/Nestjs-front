9f17acf623a9a7fa5d47de73a51a4038056f7c58
SSKyun <tbvjzoshszld@gmail.com>
2023-03-16 17:18:10 +0900

    test

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -48,8 +48,8 @@ export default function NavBar(){
           <nav className="border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
               <a href="/" className="flex items-center">
-                <img src="" className="h-6 mr-3 sm:h-9" alt="Logo" />
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">MaCo</span>
+                {/* <img src="" className="h-6 mr-3 sm:h-9" alt="Logo" /> */}
+                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">アグリート</span>
               </a>
               <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
